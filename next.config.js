5e396686f32fad005680194b26415d28ffe6cde7
michael03nyamwaya <michaelnyamwaya03@gmail.com>
2025-09-04 11:30:31 +0300

    my commit

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    DARAJA_CONSUMER_KEY: process.env.DARAJA_CONSUMER_KEY,
-    DARAJA_CONSUMER_SECRET: process.env.DARAJA_CONSUMER_SECRET,
-    DARAJA_BUSINESS_SHORTCODE: process.env.DARAJA_BUSINESS_SHORTCODE,
-    DARAJA_PASSKEY: process.env.DARAJA_PASSKEY,
-    BASE_URL: process.env.BASE_URL,
     MIKROTIK_HOST: process.env.MIKROTIK_HOST,
     MIKROTIK_USER: process.env.MIKROTIK_USER,
-    MIKROTIK_PASSWORD: process.env.MIKROTIK_PASSWORD,
-    MIKROTIK_PORT: process.env.MIKROTIK_PORT || 8728
-  }
+    MIKROTIK_PASS: process.env.MIKROTIK_PASS,
+    MIKROTIK_PORT: process.env.MIKROTIK_PORT?.toString(), // Convert to string
+  },
 }
 
 module.exports = nextConfig
